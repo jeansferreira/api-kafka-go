60f7a5ea6804c33e6eabeb044c8f265c2f506a98
Jean Ferreira <jeansferreira@gmail.com>
2023-01-15 20:56:34 -0300

    fix: modify configuration gitaction

--- env/globals.go
+++ env/globals.go
@@ -1,22 +1,21 @@
 package env
 
 import (
-	"os"
 	"fmt"
+	"os"
 	"strings"
-
 )
 
 var KAFKA_BROKERS = []string{}
-var PLAT_USER = "jean.ferreira"
-var PLAT_PASSWORD = "3qxBJMEZ9dI8JRV54HGubdzcV9omab6b"
-var CLIENTS_URL = "clients.k8s.platform.linximpulse.net"
+var PLAT_USER = os.Getenv("PLAT_USER")         //"jean.ferreira"
+var PLAT_PASSWORD = os.Getenv("PLAT_PASSWORD") //"3qxBJMEZ9dI8JRV54HGubdzcV9omab6b"
+var CLIENTS_URL = os.Getenv("CLIENTS_URL")     //"clients.k8s.platform.linximpulse.net"
 
 func SetupEnv() {
 	KAFKA_BROKERS = strings.Split(os.Getenv("KAFKA_BROKERS"), ",")
-	PLAT_USER = os.Getenv("PLAT_USER")
-	PLAT_PASSWORD = os.Getenv("PLAT_PASSWORD")
-	CLIENTS_URL = os.Getenv("CLIENTS_URL")
+	// var PLAT_USER = os.Getenv("PLAT_USER")
+	// var PLAT_PASSWORD = os.Getenv("PLAT_PASSWORD")
+	// var CLIENTS_URL = os.Getenv("CLIENTS_URL")
 
 	fmt.Println(PLAT_USER)
 	fmt.Println(PLAT_PASSWORD)
--- schemas/product.go
+++ schemas/product.go
@@ -1,6 +1,6 @@
 package schemas
 
-type HomeEvent struct {
+type ProductEvent struct {
 	ApiKey        string         `json:"apiKey" validate:"required"`
 	Source        string         `json:"source" validate:"required,oneof=desktop mobile app"`
 	SecretKey     *string        `json:"secretKey"`
@@ -12,4 +12,11 @@ type HomeEvent struct {
 	DeviceId      *string        `json:"deviceId"`
 	Info          *Info          `json:"info"`
 	AnonymousUser *AnonymousUser `json:"anonymousUser"`
+	Categories    []string       `json:"categories"`
+
+	Pid          string  `json:"pid" validate:"required"`
+	Price        float64 `json:"price" validate:"required,gt=0"`
+	Partner      *string `json:"partner"`
+	SalesChannel *string `json:"salesChannel"`
+	Status       string  `json:"status" validate:"required,oneof=available AVAILABLE unavailable UNAVAILABLE removed REMOVED"`
 }
