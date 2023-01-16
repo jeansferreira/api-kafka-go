535d46604ffc0034602c7d34a9376586828f978b
Jean Ferreira <jeansferreira@gmail.com>
2023-01-15 21:03:45 -0300

    fix: remove variables

--- env/globals.go
+++ env/globals.go
@@ -7,9 +7,9 @@ import (
 )
 
 var KAFKA_BROKERS = []string{}
-var PLAT_USER = os.Getenv("PLAT_USER")         //"jean.ferreira"
-var PLAT_PASSWORD = os.Getenv("PLAT_PASSWORD") //"3qxBJMEZ9dI8JRV54HGubdzcV9omab6b"
-var CLIENTS_URL = os.Getenv("CLIENTS_URL")     //"clients.k8s.platform.linximpulse.net"
+var PLAT_USER = os.Getenv("PLAT_USER")
+var PLAT_PASSWORD = os.Getenv("PLAT_PASSWORD")
+var CLIENTS_URL = os.Getenv("CLIENTS_URL")
 
 func SetupEnv() {
 	KAFKA_BROKERS = strings.Split(os.Getenv("KAFKA_BROKERS"), ",")
